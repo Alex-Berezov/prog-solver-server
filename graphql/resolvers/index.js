6aa2ad10b334d4637f8ef54a2e147393eb5c0607
Aleksey Berezov <a.berezov@leningrad.io>
2022-10-22 16:03:26 +0400

    fixed date

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -180,8 +180,8 @@ const root = {
       if (taskFetched) throw 'Task already exist!'
 
       const date = new Date()
-      const day = date.getDate() <= 9 ? `0${date.getDate()}` : `${date.getDate()}`
-      const month = date.getMonth() <= 9 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`
+      const day = date.getDate() <= 8 ? `0${date.getDate()}` : `${date.getDate()}`
+      const month = date.getMonth() <= 8 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`
       const year = `${date.getFullYear()}`
       const created = `${day}.${month}.${year}`
 
